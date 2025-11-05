49d76e7a9a7ec6d155a0f46bb43d78e8773fac36
j835111 <chuehwen_lin@nxn.com.tw>
2025-11-05 10:49:47 +0800

    feat: migrate configuration from PHP to Symfony YAML
    - Created config/packages/scramble.yaml with all settings
    - Added missing config parameters to Configuration class:
      - export_path
      - enum_cases_description_strategy
      - enum_cases_names_strategy
      - flatten_deep_query_parameters
    - Updated ScrambleExtension to set all parameters
    - Maintains backward compatibility with original config
    
    Related to: Phase 3 (Configuration Migration)

--- src/DependencyInjection/ScrambleExtension.php
+++ src/DependencyInjection/ScrambleExtension.php
@@ -21,13 +21,16 @@ class ScrambleExtension extends Extension
         );
         $loader->load('services.yaml');
 
-        // Set configuration parameters
+                // Set config as container parameters
         $container->setParameter('scramble.api_path', $config['api_path']);
         $container->setParameter('scramble.api_domain', $config['api_domain']);
         $container->setParameter('scramble.export_path', $config['export_path']);
         $container->setParameter('scramble.info', $config['info']);
         $container->setParameter('scramble.ui', $config['ui']);
         $container->setParameter('scramble.servers', $config['servers']);
+        $container->setParameter('scramble.enum_cases_description_strategy', $config['enum_cases_description_strategy']);
+        $container->setParameter('scramble.enum_cases_names_strategy', $config['enum_cases_names_strategy']);
+        $container->setParameter('scramble.flatten_deep_query_parameters', $config['flatten_deep_query_parameters']);
         $container->setParameter('scramble.middleware', $config['middleware']);
         $container->setParameter('scramble.extensions', $config['extensions']);
     }
